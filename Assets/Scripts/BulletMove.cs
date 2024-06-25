13db9a988d9c149937ca40a411bbdbbbc64b2aff
Popcorn0212 <cwd8564@gmail.com>
2024-06-25 15:30:29 +0900

    240625

--- Assets/Scripts/BulletMove.cs
+++ Assets/Scripts/BulletMove.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BulletMove : MonoBehaviour
 {
+
     public float moveSpeed = 10;
 
 
@@ -17,7 +18,14 @@ public class BulletMove : MonoBehaviour
         // 위로 계속 이동하고 싶다.
         // 방향: 위로, 이동속력: float, public
         // 이동공식: p = p0 + vt, p += vt
-        transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+
+        // 월드 방향 
+        Vector3 worldDir = Vector3.up;
+
+        // 로컬 방향
+        Vector3 localDir = transform.up;
+
+        transform.position += localDir * moveSpeed * Time.deltaTime;
         //transform.position += new Vector3(0, 1, 0) * moveSpeed * Time.deltaTime;
 
     }
